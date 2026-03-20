63498877edebe4b2dd8d2247e2485cf671ecdc30
gabotdev <gabrieltorres2014@gmail.com>
2026-03-20 16:30:23 -0400

    Setting up environment

---
+++ stayke-front/scripts/transformBase64.ts
@@ -0,0 +1,13 @@
+import { Keypair } from "@solana/web3.js";
+
+import bs58 from "bs58";
+
+const privateKeyBase58 = "";
+
+// Create a Keypair from the private key
+
+const keypair = Keypair.fromSecretKey(bs58.decode(privateKeyBase58));
+
+console.log("Private Key Array:", Array.from(keypair.secretKey));
+
+console.log("Base58 Encoded:", bs58.encode(keypair.secretKey)); // Should match original

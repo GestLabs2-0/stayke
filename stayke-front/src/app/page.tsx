b17d934f5346d60702bd9532bb57d74e733e845a
DeiNas77 <deinas.44Remastered@gmail.com>
2026-03-21 00:57:12 -0500

    STK-02 | WIP: integration hero section

--- stayke-front/src/app/page.tsx
+++ stayke-front/src/app/page.tsx
@@ -1,11 +1,11 @@
 //Own Components
-// import { HeroSection } from "../components/HeroSection";
+import { HeroSection } from "../components/HeroSection";
 // import { LocaleSection } from "../components/LocaleSection/LocaleSection";
 
 export default function Home() {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       {/* <LocaleSection /> */}
     </>
   );
